8d003fb02293e08cd293d49256f025a3d722d19a
SirTeruki <gregor-alexander.giotas@uni-oldenburg.de>
2019-04-15 12:10:42 +0200

    Initial commit for 2.3

---
+++ src/main/java/_2/_3/Out.java
@@ -0,0 +1,30 @@
+package _2._3;
+
+public class Out {
+
+    public static void out(boolean b) {
+        System.out.println(b);
+    }
+
+    public static void out(int i) {
+        System.out.println(i);
+    }
+
+    public static void out(double d) {
+        System.out.println(d);
+    }
+
+    public static void out(char c) {
+        System.out.println(c);
+    }
+
+    public static void out(String s) {
+        System.out.println(s);
+    }
+
+    public static void out(Object o) {
+        System.out.println(o);
+    }
+
+
+}
