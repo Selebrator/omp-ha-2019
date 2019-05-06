02e4f164a2b8ad0e5ab8361f0a9143065a4fbd92
Selebrator <l.panneke@googlemail.com>
2019-05-06 23:05:21 +0200

    Added comment about forcing bad practices

--- src/main/java/_4/_3/ComparableTest.java
+++ src/main/java/_4/_3/ComparableTest.java
@@ -1,10 +1,17 @@
 package _4._3;
 
+/*
+ * Wenn da nicht die Vorgabe mit der Ausgabe
+ * auf die Konsole wäre könnte man hierraus
+ * einen schönen UnitTest machen. Wir können
+ * uns aber natürlich auch schlechtes Verhalten
+ * angewöhnen...
+ */
 public class ComparableTest {
 	public static void main(String[] args) {
 		ComparableInteger[] array = new ComparableInteger[5];
 
-		for (int i=0; i<array.length; i++) {
+		for(int i = 0; i < array.length; i++) {
 			array[i] = new ComparableInteger(i);
 		}
 
