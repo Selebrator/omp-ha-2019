4fabc31ce0174cd8811ef8628d56bfb69bddac9f
Lisa <lisa-maria.schrader@uni-oldenburg.de>
2019-05-06 17:17:39 +0200

    Added 3.1 c)

--- src/main/java/_4/_3/Utils.java
+++ src/main/java/_4/_3/Utils.java
@@ -17,4 +17,16 @@ public class Utils {
 		}
 		return min;
 	}
+
+	public static void main(String[] args) {
+		ComparableInteger[] array = new ComparableInteger[5];
+
+		for (int i=0; i<array.length; i++) {
+			array[i] = new ComparableInteger(i);
+		}
+
+		ComparableInteger minimum = (ComparableInteger) getMinimum(array);
+		System.out.println(minimum.value); // Erwartet: 0
+
+	}
 }
