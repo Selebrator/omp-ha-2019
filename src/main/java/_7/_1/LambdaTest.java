67280ac2cd47a6c3974777435d397654f4290b18
Lisa <lisa-maria.schrader@uni-oldenburg.de>
2019-05-24 15:47:31 +0200

    improvements 7

--- src/main/java/_7/_1/LambdaTest.java
+++ src/main/java/_7/_1/LambdaTest.java
@@ -3,27 +3,26 @@ package _7._1;
 public class LambdaTest {
 
     public static void main(String[] args) {
-        Function<Double,Double> id = (x) -> x;
+        Function<Double, Double> id = (x) -> x;
 
-        Function<Double,Double> inverse = (x) -> x*-1;
+        Function<Double, Double> inverse = (x) -> -x;
 
-        Function<Double,Double> timesTen = (x) -> x*10;
+        Function<Double, Double> timesTen = (x) -> 10*x;
 
-        Function<Double,Double> divideByPi = (x) -> x/Math.PI;
+        Function<Double, Double> divideByPi = (x) -> x/Math.PI;
 
-        Function<Double,Long> round = Math::round;
-
+        Function<Double, Long> round = Math::round;
 
         @SuppressWarnings("unchecked")
-        Function<Double, Double> chain = makeChain(new Function[] {inverse, id, timesTen, divideByPi});
+        Function<Double, Double> chain = makeChain(new Function[]{ inverse, id, timesTen, divideByPi });
 
-        System.out.println(round.calculate((chain.calculate(5.5))));
+        System.out.println(round.calculate(chain.calculate(5.5)));
     }
 
     private static Function<Double, Double> makeChain(final Function<Double, Double>[] functions) {
-        return  (x) -> {
-            for (Function<Double, Double> i : functions) {
-                x = i.calculate(x);
+        return (x) -> {
+            for (Function<Double, Double> f : functions) {
+                x = f.calculate(x);
             }
             return x;
         };
--- src/main/java/_7/_2/StreamTest.java
+++ src/main/java/_7/_2/StreamTest.java
@@ -5,16 +5,7 @@ import java.util.stream.Stream;
 public class StreamTest {
     public static void main(String[] args) {
         Stream<Integer> naturals = Stream.iterate(1, i -> i + 1);
-
-        Stream<Integer> integers = Stream.iterate(0, i -> {
-            int next;
-            if (i>0) {
-                next = i*(-1);
-            } else {
-                next = i*(-1)+1;
-            }
-            return next;
-        });
+        Stream<Integer> integers = Stream.iterate(0, i -> i > 0 ? -i : -i + 1);
 
         System.out.println(filterAndSum(naturals));
         System.out.println(filterAndSum(integers));
@@ -23,7 +14,7 @@ public class StreamTest {
     private static Integer filterAndSum(Stream<Integer> stream) {
         return stream.filter((i) -> i % 2 == 0)
                 .limit(10)
-                .reduce((a,b) -> a+b)
+                .reduce(Integer::sum)
                 .orElse(0);
     }
 }
